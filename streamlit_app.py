789a00a9827c3aa6c2ddc726a4694de63ee4b2e2
niladridey1 <31997137+niladridey1@users.noreply.github.com>
2023-03-29 00:32:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,25 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+def answer_question(question):
+    prompt = f"Question: {question}\nAnswer:"
 
-"""
-# Welcome to Streamlit!
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024,
+        n=1,
+        stop="\n",
+        timeout=10,
+    )
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    answer = response.choices[0].text.strip()
+    return answer
+@app.route("/process_form", methods=["POST"])
+def process_form():
+    question = request.form.get("question")
+    answer = answer_question(question)
+    return render_template("result.html", question=question, answer=answer)
